6d21164721884384c884aaad4fee6a6499dba235
Divya Chahal <divyachahal@outlook.com>
2022-07-11 00:42:50 +0100

    First commit

---
+++ Add.cpp
@@ -0,0 +1,27 @@
+/*
+ * Add.cpp
+ *
+ *  Created on: 11-Jul-2022
+ *      Author: divyachahal
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a,b,c;
+
+	  cout<< "Enter a , b\n";
+
+	  cin>>a>>b;
+
+	  c=a+b;
+
+	  cout<<"Sum is:"<<c<<endl;
+
+	return 0;
+}
+
+
+
---
+++ Multiply.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : CPP.cpp
+// Author      : Divya Chahal
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "First CPP Program on Mac" << endl; // prints !!!Hello World!!!
+
+	int a,b,c;
+
+	  cout << "Hello World!\n";
+
+	  cout<< "Enter a , b\n";
+
+	  cin>>a>>b;
+
+	  c=a*b;
+
+	  cout<<"Multiple of:"<<a<<"and"<<b<<" is "<<c<<endl;
+
+	return 0;
+}
